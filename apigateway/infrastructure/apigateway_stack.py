4f7f6dbdbed02d721da3569dd9f516a77522b42b
gitName <a@b.c>
2022-12-16 15:20:29 -0500

    Removing edit from player parameters

--- apigateway/infrastructure/apigateway_stack.py
+++ apigateway/infrastructure/apigateway_stack.py
@@ -84,7 +84,6 @@ class ApigatewayStack(Stack):
                                                                           "Source": "ingest-api",
                                                                           "DetailType": "player",
                                                                           "Detail": "$request.body",
-                                                                          "Method": "$request.method",
                                                                           # Plan to add timestamp as key / value pair passed in
                                                                           "EventBusName": core_event_bus.event_bus_arn
                                                                       },
