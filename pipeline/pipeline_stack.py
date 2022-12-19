23fb701e67b50b2be0fafe398f561f6dce436979
gitName <a@b.c>
2022-12-19 09:40:16 -0500

    Updating branch in pipeline

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -20,7 +20,7 @@ class PipelineStack(Stack):
             synth=pipelines.ShellStep('Synth',
                                       input=pipelines.CodePipelineSource.git_hub(
                                           'ttrain45/ingest-domain',
-                                          'feature/edit-player',
+                                          'feature/delete-player',
                                           authentication=SecretValue.secrets_manager(
                                               'exploration-token')
                                       ),
