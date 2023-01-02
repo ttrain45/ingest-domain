e86b4ec5daa67404888592d94c3e3f2bf7aeaf7a
gitName <a@b.c>
2023-01-02 11:51:17 -0500

    Adding logging to rest gateway

--- apigateway/infrastructure/apigateway_stack.py
+++ apigateway/infrastructure/apigateway_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     Stack,
     aws_events_targets as target,
     aws_lambda as _lambda,
+    aws_logs as logs,
     Duration,
     aws_iam as iam,
     aws_apigateway as apigateway,
@@ -20,7 +21,16 @@ class ApigatewayStack(Stack):
     def __init__(self, scope, id: str, handler_arn, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        rest_api = apigateway.RestApi(self, "IngestGateway")
+        rest_api_log_group = logs.LogGroup(self, "IngestLogs")
+
+        rest_api = apigateway.RestApi(
+            self,
+            "IngestGateway",
+            deploy_options=apigateway.StageOptions(
+                access_log_destination=apigateway.LogGroupLogDestination(rest_api_log_group),
+                access_log_format=apigateway.AccessLogFormat.clf()
+            )
+        )
 
         handler = python.PythonFunction.from_function_arn(self, "IngestLambdaHandler", handler_arn)
 
